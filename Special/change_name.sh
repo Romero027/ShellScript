d3a7c73146909fc4845b5bd8b0229b6b639b1983
xzhu27 <xzhu27@ucsc.edu>
2017-11-07 10:35:56 -0800

    update

---
+++ Special/change_name.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#For more information about read:http://linuxtechlab.com/9-bash-scripting-user-input-read-command-redirection/
+read -p "Please enter a file extension: " EXTENSION
+read -pamPlease enter a file prefix: (Press ENTER for 2015­08­10). vacation
