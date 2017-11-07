20edeb6cc7ac0ef4d191a24d07ea2992f7d5ea65
xzhu27 <xzhu27@ucsc.edu>
2017-11-07 10:28:10 -0800

    update

--- Special/date.sh
+++ Special/date.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#gracefully handle instances where there are no ".jpg" files in the
+#current directory. 
+
+#-s
+#Enable (set) each optname.
+#nullglob
+#If set, Bash allows filename patterns which match no files to expand to a null string,
+#rather than themselves.
+
+
+shopt -s nullglob
 
 for FILE in *.jpg
 do	
