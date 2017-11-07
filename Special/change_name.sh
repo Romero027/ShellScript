1357e1489e8358af66bd16d8f30eca6a527f5699
xzhu27 <xzhu27@ucsc.edu>
2017-11-07 10:50:07 -0800

    update

--- Special/change_name.sh
+++ Special/change_name.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #For more information about read:http://linuxtechlab.com/9-bash-scripting-user-input-read-command-redirection/
+
+# YYYY-MM-DD
 DATE=$(date +%F)
+
 read -p "Please enter a file extension: " EXTENSION
-read -p "Please enter a file prefix: (Press ENTER for ${DATE} " PREFIX
+read -p "Please enter a file prefix:  (Press ENTER for ${DATE}). " PREFIX
+
+# quote may be removed
+if [ -z "$PREFIX" ]
+then
+  PREFIX="$DATE"
+fi
 
 for FILE in *.${EXTENSION}
 do
-	if [ -z PREFIX ]
-		then 
-			echo "Renaming $FILE to $(date +%F)-${FILE}"
-			mv $FILE $(date +%F)-${FILE}
-	else
-		echo "Renaming $FILE to ${PREFIX}-${FILE}"
-		mv $FILE ${PREFIX}-${FILE}
-	fi
+  NEW_FILE="${PREFIX}-${FILE}"
+  echo "Renaming $FILE to ${NEW_FILE}."
+  mv $FILE ${NEW_FILE}
 done
