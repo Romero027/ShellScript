048edfe38f57c2745ff9d35575bc5568c26844af
xzhu27 <xzhu27@ucsc.edu>
2017-11-08 12:27:26 -0800

    update

---
+++ debugging/debug-e.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+# -e = exit on error(none 0 return value)
+FILE_NAME="/not/here"
+ls $FILE_NAME
+echo $FILE_NAME
---
+++ debugging/debug-v.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -v
+#We can combine several options together
+# -v = prints shell input lines as they are read
+
+TEST_VAR="test"
+echo "$TEST_VAR"
